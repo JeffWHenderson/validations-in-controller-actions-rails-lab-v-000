20697e27aaa4198a129d1ef61834147b2db8ba27
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-21 16:19:36 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    if   @post.update(post_params)#@post.valid?
+    raise  @post.update(post_params).inspect
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
